8ea1328ce14e3508f09cd9271554737dc20df38f
danylott <d.tuytuyshkin@gmail.com>
2022-11-15 12:59:42 +0200

    initialize app and models

--- rick_and_morty/settings.py
+++ rick_and_morty/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "characters",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+RICK_AND_MORTY_API_CHARACTERS_URL = "https://rickandmortyapi.com/api/character"
